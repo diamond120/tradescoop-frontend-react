ed057b7e1ad4b36833f2d383d1c53145323a2e4b
mrcordova <46950394+mrcordova@users.noreply.github.com>
2025-02-12 13:53:05 -0800

    form validity and post comment

---
+++ js/blog-post-script.js
@@ -0,0 +1,24 @@
+const submitCommentBtn = document
+  .querySelector("[data-comment-btn]")
+  .addEventListener("click", (e) => {
+    e.preventDefault();
+    const form = e.currentTarget.closest("form");
+    const name = form.name.value;
+    const email = form.email.value;
+    const comment = form.comment.value;
+
+    if (!form.checkValidity()) {
+      form.reportValidity();
+      return;
+    }
+    form.insertAdjacentHTML(
+      "beforebegin",
+      `<ul>
+      <li><h3>Thanks for your comment!</h3></li>
+        <li>name: ${name}</li>
+        <li>email: ${email}</li>
+        <li>comment: ${comment}</li>
+        </ul>`
+    );
+    form.remove();
+  });
